338cc37762998403d9d74be95f833114689fb629
Harry Eakins <harry@goklue.com>
2019-08-11 21:39:42 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,16 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "CSwiftV"
+    name: "CSwiftV",
+    products: [
+        .library(name: "CSwiftV", targets: ["CSwiftV"]),
+    ],
+    targets: [
+      .target(
+          name: "CSwiftV",
+          dependencies: [],
+          path: "Sources"
+      )
+    ]
 )
-
